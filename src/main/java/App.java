b3dec3322b5b6d4fb7ff4a897d39a66622358ec3
Wailo0 <iymashev@gmail.com>
2023-01-18 13:39:09 +0300

    c1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.equals(bean1));
+
+
+        Cat cat1 = (Cat) applicationContext.getBean(Cat.class);
+        Cat cat2 = (Cat) applicationContext.getBean(Cat.class);
+        System.out.println(cat1.equals(cat2));
     }
 }
